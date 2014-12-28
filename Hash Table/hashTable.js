80641c0c04d160d11a49fd5860fe8965f8b64f62
Richard VanBreemen <rvbsanjose@me.com>
2014-12-27 16:25:41 -0800

    Updated hash table

--- Hash Table/hashTable.js
+++ Hash Table/hashTable.js
@@ -6,31 +6,31 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
 	var i = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(i);
+	var bucket = this._storage.get(i);
 
-  this._count++;
-  if (bucket) {
-  	var tuple = _.find(bucket, function (tuple) {
-  		return tuple[0] === k;
-  	});
-  	if (tuple) tuple[1] = v
-  	else {
-  		bucket.push([k, v]);
-  		if (this._count > (this._limit * 0.75))
-  			this.resize(this._limit * 2);
-  	}
-  } else this._storage.set(i, [[k, v]]);
+	this._count++;
+	if (bucket) {
+		var tuple = _.find(bucket, function (tuple) {
+			return tuple[0] === k;
+		});
+		if (tuple) tuple[1] = v
+		else {
+			bucket.push([k, v]);
+			if (this._count > (this._limit * 0.75))
+				this.resize(this._limit * 2);
+		}
+	} else this._storage.set(i, [[k, v]]);
 };
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(i);
-  if (bucket) {
-  	var tuple = _.find(bucket, function (tuple) {
-  		return tuple[0] === k;
-  	});
-  	if (tuple) return tuple[1];
-  } else return null;
+	var i = getIndexBelowMaxForKey(k, this._limit);
+	var bucket = this._storage.get(i);
+	if (bucket) {
+		var tuple = _.find(bucket, function (tuple) {
+			return tuple[0] === k;
+		});
+		if (tuple) return tuple[1];
+	} else return null;
 };
 
 HashTable.prototype.remove = function(k){
